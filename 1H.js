f0ba00baadd69611137bea80de117e187d5fc070
Anastasia <anastasia.chelpanova@bts.tech>
2023-06-01 19:22:58 +0200

    add comment

--- 1H.js
+++ 1H.js
@@ -2,3 +2,10 @@ function concatString(string1, string2){
     return string1 + string2
 }
 console.log (concatString("pri", "vet"))
+
+// return vs console.log
+// return - return result of the function
+//console.log - print in Terminal
+
+//When would you choose to use functions over the way we have been scripting so far?
+//code organisation, reusabitity, modality
